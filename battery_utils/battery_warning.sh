fdc357b21f3e33e270bfec4d2b00c1ee4dc7bf62
Cristobal Lopez <crolopezp@gmail.com>
2022-10-07 11:34:24 -0700

    Add an improvement to the battery checking logic

--- battery_utils/battery_warning.sh
+++ battery_utils/battery_warning.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 check_battery() {
-	ok_msg="You can keep charging your battery."
-	err_msg="Remember to disconnect the battery cable!"
 	bat_perc=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk -F ":" '{ print $2 }' | tr -d "%" | tr -d " ")
 	if [ $bat_perc -gt 80 ]; then
-		notify-send $err_msg
+		notify-send "Remember to disconnect the battery cable!"
 	elif [ $bat_perc -lt 40 ]; then
-		notify-send $err_msg
+		notify-send "Remember to connect the battery cable!"
 	else
 		# Only print to stdout if run in a terminal
-		echo $ok_msg
+		echo "You can keep charging your battery."
 	fi
 }
 
 add_task_to_cron() {
-	#cp first @TODO 
+	#cp first @TODO
 	echo "*/20 * * * * ls" | tee -a /var/spool/cron/root
 	# Study feasibility
 	{
